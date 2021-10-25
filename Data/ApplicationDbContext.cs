b258b367cdab7b7e330f4dc520840d81eadaff38
echao12 <echao0077@gmail.com>
2021-10-25 14:09:19 -0700

    updated database with Items table
    Added the Items table to the context & added the Items migration to update the database.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Data
 {
+    //a context class provides an entry point to the database.
+    //defines how the software will interact with the database.
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        //DbSet is a table in the database
+        //creating items to hold a table(DbSet) of ToDoItems.
+        public DbSet<ToDoItem> Items{ get; set; }
+        //this db context now has an Items table, but the actual db does not.
+        //need to create a migration to update the database.
+        //migrations track changes to db structure over time & make it possible
+        //to roll back changes or clone the database.
+        //create migration: dotnet ef migrations add AddItems
+        //update database with the Items table: dotnet ef database update
     }
 }
